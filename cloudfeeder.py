266487cb1250c313c04fc50db74f07f6632781a2
Junhyung Ki <kijoonh91@gmail.com>
2020-08-17 17:38:44 +0200

    minor

--- cloudfeeder.py
+++ cloudfeeder.py
@@ -40,8 +40,8 @@ tclient.do_connect("--insecure")
 super_admin_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJrdWtzYS52YWwiLCJpc3MiOiJFY2xpcHNlIEtVS1NBIERldiIsImFkbWluIjp0cnVlLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6MTYwNjIzOTAyMiwidzNjLXZzcyI6eyIqIjoicncifX0.bUcEW4o3HiBHZAdy71qCWcu9oBSZClntI1ZXq7HAM8i8nDtiUP4up-VXxt3S3n8AKJQOZVlHudP_ixGTb1HBKa3_CD0HFurP_Wf2Jnrgguhqi1sUItvGjgq4BPpuBsu9kV1Ds-JDmCPBBuHfRtCck353BmMyv6CHn2CCgVQH-DwW6k7wOqcFkxfxfkClO-nrUSQbad_MrxNHhrnflb3N8bc4r61BQ8gHiEyl7JJIuLhAb7bLgarjqcHABkw6T2TkwfWFsddMR2GL_PYBP4D3_r-2IHAhixiEiO758lxA2-o2D0jtte-KmTHjeEUpaWr-ryv1whZXnE243iV1lMajvjgWq5ZnsYTG4Ff7GsR_4SKyd9j6wInkog5Kkx5tFJr2P9kh7HupXQeUzbuoJ7lZAgpGyD8icxZg7c8VTpLzTs5zowjJwbxze5JAylWcXLXOA3vQKpn8E3MseD_31LoVZGEvD9p372IgVmJ0ui4qT8_ZHeGPc8bV2Iy0vDkdAhjf-4Lwf4rDGDksYpK_PO70KylGRmZ9TqiKqstUI6AWG50Jii8MPnnr8qyNO3FD8Rv7E8BnL8ioLoN5VI9eyxy1HpW2SfLKUuCaLB9iKd6fv4U_DhF1AS-Y-iu8-kOovxkTk801DhDxWJN0nyRwmhqn8exjikNB1jnW5mFWLTeagNA"
 tclient.do_authorize(super_admin_token)
 
+sigDictCH0 = {}
 sigDictCH1 = {}
-sigDictCH2 = {}
 while True:
 	# 1. Save signals' values from the target path signal dictionaries
 	ambientAirTemp = checkPath("Vehicle.AmbientAirTemperature")
@@ -69,60 +69,60 @@ while True:
 	
 	# 2. Store newly retrieved data to the dictionary keys
 	## A. Calculate integrated NOx mass
-	sigDictCH1["Aftrtratment1ExhaustGasMassFlow"] = exhaustMessFlow
-	sigDictCH2["Aftertreatment1IntakeNOx"] = noxIntake 
-	sigDictCH2["Aftertreatment1OutletNOx"] = noxOutlet
+	sigDictCH0["Aftrtratment1ExhaustGasMassFlow"] = exhaustMessFlow
+	sigDictCH1["Aftertreatment1IntakeNOx"] = noxIntake 
+	sigDictCH1["Aftertreatment1OutletNOx"] = noxOutlet
 	## B. Calculate engine work
-	sigDictCH1["EngReferenceTorque"] = referenceTorque # Missing
+	sigDictCH0["EngReferenceTorque"] = referenceTorque # Missing
 	## C. Map switch over
-	sigDictCH1["AmbientAirTemp"] = ambientAirTemp
-	sigDictCH1["BarometricPress"] = barometricPress
-	sigDictCH1["EngCoolantTemp"] = coolantTemp
+	sigDictCH0["AmbientAirTemp"] = ambientAirTemp
+	sigDictCH0["BarometricPress"] = barometricPress
+	sigDictCH0["EngCoolantTemp"] = coolantTemp
 	## D. Bin selection
-	sigDictCH1["EngPercentLoadAtCurrentSpeed"] = loadAtCurrentSpeed
-	sigDictCH1["EngSpeedAtIdlePoint1"] = engSpeedAtIdle # Missing
-	sigDictCH1["EngSpeedAtPoint2"] = engSpeedAtKickIn # Missing
+	sigDictCH0["EngPercentLoadAtCurrentSpeed"] = loadAtCurrentSpeed
+	sigDictCH0["EngSpeedAtIdlePoint1"] = engSpeedAtIdle # Missing
+	sigDictCH0["EngSpeedAtPoint2"] = engSpeedAtKickIn # Missing
 	## A & B & C
-	sigDictCH1["EngSpeed"] = engSpeed
+	sigDictCH0["EngSpeed"] = engSpeed
 	## B & D
-	sigDictCH1["ActualEngPercentTorque"] = actualTorque
-	sigDictCH1["NominalFrictionPercentTorque"] = nomFrictionTorque
+	sigDictCH0["ActualEngPercentTorque"] = actualTorque
+	sigDictCH0["NominalFrictionPercentTorque"] = nomFrictionTorque
 	## C - case 2 & Sampling duration tracking per bin
-	sigDictCH1["TimeSinceEngineStart"] = timeSinceStart # Missing
+	sigDictCH0["TimeSinceEngineStart"] = timeSinceStart # Missing
 	
 	# 3. Print the corresponding variable's value
-	printSignalValues(sigDictCH1, sigDictCH2)
+	printSignalValues(sigDictCH0, sigDictCH1)
 	
 	# 4. Select a mapping (bad / intermediate / good) (cold / hot start)
 	# * Fault active part is omitted
 	## A. New Concept
 	### barometric (kpa): mbar = 10 x kPa
 	### * Assuming T_SCR == EngCoolantTemp (THIS IS PROBABLY NOT CORRECT)
-	if (sigDictCH1["TimeSinceEngineStart"] < 180 
-		or sigDictCH1["AmbientAirTemp"] < -7 
-		or sigDictCH1["BarometricPress"] * 10 < 750 
-		or sigDictCH1["EngCoolantTemp"] < 180):
+	if (sigDictCH0["TimeSinceEngineStart"] < 180 
+		or sigDictCH0["AmbientAirTemp"] < -7 
+		or sigDictCH0["BarometricPress"] * 10 < 750 
+		or sigDictCH0["EngCoolantTemp"] < 180):
 		print("Bad Mapping")
-	elif (sigDictCH1["TimeSinceEngineStart"] >= 180 
-		and sigDictCH1["AmbientAirTemp"] >= -7 
-		and sigDictCH1["BarometricPress"] * 10 >= 750):
-		if 180 <= sigDictCH1["EngCoolantTemp"] < 220:
+	elif (sigDictCH0["TimeSinceEngineStart"] >= 180 
+		and sigDictCH0["AmbientAirTemp"] >= -7 
+		and sigDictCH0["BarometricPress"] * 10 >= 750):
+		if 180 <= sigDictCH0["EngCoolantTemp"] < 220:
 			print("Intermediate Mapping")
-		elif sigDictCH1["EngCoolantTemp"] >= 220:
+		elif sigDictCH0["EngCoolantTemp"] >= 220:
 			print("Good Mapping")
 	## B. Old Concept
 	# * Fault active part is omitted
-	if (sigDictCH1["TimeSinceEngineStart"] >= 1800
-		and sigDictCH1["AmbientAirTemp"] >= -7
-		and sigDictCH1["BarometricPress"] * 10 >= 750):
+	if (sigDictCH0["TimeSinceEngineStart"] >= 1800
+		and sigDictCH0["AmbientAirTemp"] >= -7
+		and sigDictCH0["BarometricPress"] * 10 >= 750):
 		print("Old Concept Good Mapping")
 	## C. PEMS Concept
-	if (sigDictCH1["TimeSinceEngineStart"] >= 60
-		and sigDictCH1["AmbientAirTemp"] >= -7 
-		and sigDictCH1["BarometricPress"] * 10 >= 750):
-		if 30 <= sigDictCH1["EngCoolantTemp"] < 70:
+	if (sigDictCH0["TimeSinceEngineStart"] >= 60
+		and sigDictCH0["AmbientAirTemp"] >= -7 
+		and sigDictCH0["BarometricPress"] * 10 >= 750):
+		if 30 <= sigDictCH0["EngCoolantTemp"] < 70:
 			print("PEMS - cold start part")
-		elif sigDictCH1["EngCoolantTemp"] >= 70:
+		elif sigDictCH0["EngCoolantTemp"] >= 70:
 			print("PEMS - hot start part")
 	
 	# 5. Time delay
@@ -157,18 +157,18 @@ while True:
 
 	# 2. Store newly retrieved data to the dictionary keys
 	####### - can0 - #######
-	sigDictCH1["AmbientAirTemp"] = ambientAirTemp
-	sigDictCH1["ActualEngPercentTorque"] = percentTorque
-	sigDictCH1["BarometricPress"] = barometricPress
-	sigDictCH1["EngAirIntakeTemp"] = airIntakeTemp
-	sigDictCH1["EngCoolantTemp"] = coolantTemp
-	sigDictCH1["EngFuelRate"] = fuelRate
-	sigDictCH1["EngPercentLoadAtCurrentSpeed"] = percentLoadAtSpeed
-	sigDictCH1["EngSpeed"] = engSpeed
-	sigDictCH1["NominalFrictionPercentTorque"] = frictionPercentTorque
+	sigDictCH0["AmbientAirTemp"] = ambientAirTemp
+	sigDictCH0["ActualEngPercentTorque"] = percentTorque
+	sigDictCH0["BarometricPress"] = barometricPress
+	sigDictCH0["EngAirIntakeTemp"] = airIntakeTemp
+	sigDictCH0["EngCoolantTemp"] = coolantTemp
+	sigDictCH0["EngFuelRate"] = fuelRate
+	sigDictCH0["EngPercentLoadAtCurrentSpeed"] = percentLoadAtSpeed
+	sigDictCH0["EngSpeed"] = engSpeed
+	sigDictCH0["NominalFrictionPercentTorque"] = frictionPercentTorque
 	####### - can1 - #######
-	sigDictCH2["Aftertreatment1IntakeNOx"] = intakeNOx
-	sigDictCH2["Aftertreatment1OutletNOx"] = outletNOx
-	sigDictCH2["NominalFrictionPercentTorque"
+	sigDictCH1["Aftertreatment1IntakeNOx"] = intakeNOx
+	sigDictCH1["Aftertreatment1OutletNOx"] = outletNOx
+	sigDictCH1["NominalFrictionPercentTorque"
 		] = frictionPercentTorqueVector
 """
