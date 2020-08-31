224b9da90938abcb0d78d8b41d06b42471b45e9a
Junhyung Ki <kijoonh91@gmail.com>
2020-08-31 17:45:24 +0200

    mqtt receiving data

--- mqtt_subscriber.py
+++ mqtt_subscriber.py
@@ -14,35 +14,40 @@ https://stackoverflow.com/questions/32540670/is-mqtt-support-both-one-to-many-an
 import paho.mqtt.client as mqtt
 import time
 import json
+import os
 
-def on_message(client, userdata, msg):
-    topic=msg.topic
-    m_decode=str(msg.payload.decode("utf-8","ignore"))
-    print("data Received type",type(m_decode))
-    print("data Received",m_decode)
-    print("Converting from Json to Object")
-    m_in=json.loads(m_decode)
-    print(type(m_in))
-    print("broker 2 address = ",m_in["broker2"])
+# def on_message(client, userdata, msg):
+    # topic=msg.topic
+    # m_decode=str(msg.payload.decode("utf-8","ignore"))
+    # print("data Received type",type(m_decode))
+    # print("data Received",m_decode)
+    # print("Converting from Json to Object")
+    # m_in=json.loads(m_decode)
+    # print(type(m_in))
+    # print("broker 2 address = ",m_in["broker2"])
 
-# Subscribing the topic (should be aligned with the publisher)
-topic = "test/json_test"
-client = mqtt.Client("test_subscriber")
-# on_message runs in a new thread
-client.on_message=on_message
-# Connecting to the MQTT broker (should be aligned with the publisher)
-# mqtt_broker = "test.mosquitto.org" # latency problem is outstanding...
-mqtt_broker = "mqtt.bosch-iot-hub.com" ### this line should be modified
-print("Connecting to broker ", mqtt_broker)
-# client.connect(mqtt_broker)
-client.connect(mqtt_broker, port=8883) ### this line should be modified
-client.loop_start()
-time.sleep(3)
+# # Subscribing the topic (should be aligned with the publisher)
+# topic = "test/json_test"
+# client = mqtt.Client("test_subscriber")
+# # on_message runs in a new thread
+# client.on_message=on_message
+# # Connecting to the MQTT broker (should be aligned with the publisher)
+# # mqtt_broker = "test.mosquitto.org" # latency problem is outstanding...
+# mqtt_broker = "mqtt.bosch-iot-hub.com" ### this line should be modified
+# print("Connecting to broker ", mqtt_broker)
+# # client.connect(mqtt_broker)
+# client.connect(mqtt_broker, port=8883) ### this line should be modified
+# client.loop_start()
+# time.sleep(3)
 
-while True:
-	print("Receiving data from the connected vehicle...")
-	time.sleep(1)
-	client.subscribe(topic)
+# Bosch IoT Hub (Eclipse Hono)
+cmd = 'mosquitto_sub -d -h mqtt.bosch-iot-hub.com -p 8883 -u pc01@t20babfe7fb2840119f69e692f184127d -P junhyungki@123 --cafile iothub.crt -k 30 -t telemetry' # message to be continued...
+os.system(cmd)
 
-client.loop_stop()
-client.disconnect()
+# while True:
+    # print("Receiving data from the connected vehicle...")
+    # time.sleep(1)
+    # client.subscribe(topic)
+
+# client.loop_stop()
+# client.disconnect()
