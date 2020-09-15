fafe474d8be5eed27f844f4517b4dbabcfba945a
junh-ki <kijoonh91@gmail.com>
2020-09-15 17:47:33 +0200

    minor

--- utils/maven.consumer.hono/src/main/java/maven/consumer/hono/ExampleConsumer.java
+++ utils/maven.consumer.hono/src/main/java/maven/consumer/hono/ExampleConsumer.java
@@ -18,9 +18,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
