4829813a1f38e7cda8049e9680a11a273d7ebec4
junh-ki <kijoonh91@gmail.com>
2020-10-02 15:16:03 +0200

    upgrade

--- utils/asc2log_channel_separater.py
+++ utils/asc2log_channel_separater.py
@@ -1,5 +1,5 @@
 """
-This script is to separate a .asc logfile by CAN channel 
+This script is to separate a .asc logfile by CAN channel (up to three channels)
 and convert the results to the .log files.
 
 Since the script uses the can-utils library,
@@ -9,18 +9,19 @@ the following command should be run first before running the script:
 
 example.asc > ch0_example.log, ch1_example.log
 
-(e.g., 'python3 asc2log_channel_separater.py -a example.asc -c vcan0')
+(e.g., 'python3 asc2log_channel_separater.py --asc example.asc -c vcan0')
 
 """
 
 import os
 import sys
 import ntpath
+import argparse
 
 def getConfig():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--asc", metavar='\b', help="Target .asc file", type=str)
-    parser.add_argument("-c", "--can", metavar='\b', help="Result CAN interface name", type=str)
+    parser.add_argument("--asc", metavar='\b', help="Target asc file", type=str)
+    parser.add_argument("--can", metavar='\b', help="Result CAN interface name", type=str)
     args = parser.parse_args()
     return args
 
@@ -44,36 +45,41 @@ can1lines = []
 can2 = "can2"
 can2lines = []
 
-for myline in data:
-	if can0 in myline:
-		myline = myline.replace(can0, interface)
-		can0lines.append(myline)            
-	elif can1 in myline:
-		myline = myline.replace(can1, interface)
-		can1lines.append(myline)
-	elif can2 in myline:
-		myline = myline.replace(can2, interface)
-		can2lines.append(myline)
+for myline in data.splitlines():
+    if can0 in myline:
+        myline = myline.replace(can0, interface)
+        can0lines.append(myline)
+    elif can1 in myline:
+        myline = myline.replace(can1, interface)
+        can1lines.append(myline)
+    elif can2 in myline:
+        myline = myline.replace(can2, interface)
+        can2lines.append(myline)
 
 # Reproduce the result filenames from the input filename
-file_name_with_format = ntpath.basename(interface)
+file_name_with_format = ntpath.basename(asc)
 split_string = file_name_with_format.split(".",1)
 file_name = split_string[0]
 
 # Create the result .log file that only contains data from CAN channel 0 (can0)
 if len(can0lines) != 0:
-    with open('ch0_' + file_name + '.log', 'w') as f:
+    with open('can0_' + file_name + '.log', 'w') as f:
         for item in can0lines:
-            f.write("%s" % item)
+            f.write("%s\n" % item)
+        f.close()
 
 # Create the result .log file that only contains data from CAN channel 1 (can1)
 if len(can1lines) != 0:
-    with open('ch1_' + file_name + '.log', 'w') as f:
+    with open('can1_' + file_name + '.log', 'w') as f:
         for item in can1lines:
-            f.write("%s" % item)
+            f.write("%s\n" % item)
+        f.close()
 
 # Create the result .log file that only contains data from CAN channel 2 (can2)
 if len(can2lines) != 0:
-    with open('ch2_' + file_name + '.log', 'w') as f:
+    with open('can2_' + file_name + '.log', 'w') as f:
         for item in can2lines:
-            f.write("%s" % item)
+            f.write("%s\n" % item)
+        f.close()
+
+print("Successfully done.")
