c146c2205ae29c1a0257e9a3b2037de3a469d609
junh-ki <kijoonh91@gmail.com>
2020-10-02 11:39:07 +0200

    name changed

--- utils/cloud/influxDB_consumer.py
+++ utils/cloud/influxDB_consumer.py
@@ -2,9 +2,9 @@ import os
 import json
 import time
 
-def storeNewMetrics(list, db, metrics):
+def storeNewMetricVal(list, db, metrics):
 	# Check the last data
-	lastVal = getLastMetrics(db, metrics)
+	lastVal = getLastMetricVal(db, metrics)
 	# Append if the derived data is not the same as the latest one in the list
 	if lastVal != None:
 		if len(list) == 0:
@@ -12,7 +12,7 @@ def storeNewMetrics(list, db, metrics):
 		elif list[-1] != lastVal:
 			list.append(lastVal)
 
-def getLastMetrics(db, metrics):
+def getLastMetricVal(db, metrics):
 	# Read the last data (descending order) from the local InfluxDB server
 	data = os.popen("curl -G 'http://localhost:8086/query?pretty=true' --data-urlencode \"db=" + db 
 		+ "\" --data-urlencode \"q=SELECT value FROM " + metrics + " ORDER BY desc LIMIT 1\"").read()
@@ -29,7 +29,7 @@ bin_pos_list = []
 work_list = []
 
 while True:
-	storeNewMetrics(nox_list, db, "cumulative_nox")
-	storeNewMetrics(bin_pos_list, db, "bin_position")
-	storeNewMetrics(work_list, db, "cumulative_work")
+	storeNewMetricVal(nox_list, db, "cumulative_nox")
+	storeNewMetricVal(bin_pos_list, db, "bin_position")
+	storeNewMetricVal(work_list, db, "cumulative_work")
 	time.sleep(1)
