660bc4a99621eb38ba9d39d7bcf476b0dcaadee8
Junhyung Ki <kijoonh91@gmail.com>
2021-01-07 11:51:08 +0100

    diagnostics part added

---
+++ utils/cloud/diagnostics/src/main/java/com/dias/diagnostics/DiagnosticsApplication.java
@@ -0,0 +1,13 @@
+package com.dias.diagnostics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosticsApplication.class, args);
+	}
+
+}
---
+++ utils/cloud/diagnostics/src/test/java/com/dias/diagnostics/DiagnosticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dias.diagnostics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
