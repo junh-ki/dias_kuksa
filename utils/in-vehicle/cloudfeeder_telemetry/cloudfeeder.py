461cd111cdf223bfc95d8870b4a08728f293277c
Junhyung Ki <kijoonh91@gmail.com>
2021-01-25 17:59:18 +0100

    resuming data feature added

--- utils/in-vehicle/cloudfeeder_telemetry/cloudfeeder.py
+++ utils/in-vehicle/cloudfeeder_telemetry/cloudfeeder.py
@@ -30,6 +30,7 @@ def getConfig():
     parser.add_argument("-P", "--password", metavar='\b', help="Credential Authorization Password / Configured in \"Bosch IoT Hub Management API\"", type=str) # "junhyungki@123"
     parser.add_argument("-c", "--cafile", metavar='\b', help="Server Certificate File (e.g., iothub.crt)", type=str) # "iothub.crt"
     parser.add_argument("-t", "--type", metavar='\b', help="Transmission Type (e.g., telemetry or event)", type=str) # "telemetry"
+    parser.add_argument("-r", "--resume", action='store_true', help="Resume the application with the accumulated data", default=False)
     args = parser.parse_args()
     return args
 
@@ -107,6 +108,53 @@ def send_telemetry(host, port, comb, telemetry_queue):
         telemetry_queue.append(comb)
         print("# The current telemetry collected, Queue Length: " + str(len(telemetry_queue)))
 
+def load_data(binPro):
+    try:
+        f = open("saved_dict.json", "r")
+        binPro.dashboard = json.load(f)
+        print("Saved dictionary data successfully loaded :)")
+    except FileNotFoundError:
+        print("Saved dictionary file not found :(")
+    except IOError:
+        print("Saved dictionary file not found :(")
+    try:
+        f = open("saved_samp.json", "r")
+        samp = json.load(f)
+        binPro.ctr_total = samp["ctr_total"]
+        binPro.ctr_tscr_bad = samp["ctr_tscr_bad"]
+        binPro.ctr_tscr_intermediate = samp["ctr_tscr_intermediate"]
+        binPro.ctr_tscr_good = samp["ctr_tscr_good"]
+        binPro.ctr_old_good = samp["ctr_old_good"]
+        binPro.ctr_pems_cold = samp["ctr_pems_cold"]
+        binPro.ctr_pems_hot = samp["ctr_pems_hot"]
+        print("Saved sampling time file successfully loaded :)")
+    except FileNotFoundError:
+        print("Saved sampling time file not found :(")
+        print("Starting from scratch.")
+    except IOError:
+        print("Saved sampling time file not found :(")
+        print("Starting from scratch.")
+
+def save_data(binPro):
+    saved_dict = json.dumps(binPro.dashboard)
+    f = open("saved_dict.json", "w")
+    f.write(saved_dict)
+    f.close()
+    # Saving sampling times
+    saved_samp = {
+        "ctr_total": binPro.ctr_total,
+        "ctr_tscr_bad": binPro.ctr_tscr_bad,
+        "ctr_tscr_intermediate": binPro.ctr_tscr_intermediate,
+        "ctr_tscr_good": binPro.ctr_tscr_good,
+        "ctr_old_good": binPro.ctr_old_good,
+        "ctr_pems_cold": binPro.ctr_pems_cold,
+        "ctr_pems_hot": binPro.ctr_pems_hot
+    }
+    saved_samp = json.dumps(saved_samp)
+    f = open("saved_samp.json", "w")
+    f.write(saved_samp)
+    f.close()
+
 print("kuksa.val cloud example feeder")
 
 # Get the pre-fix command for publishing data
@@ -117,6 +165,9 @@ client = getVISSConnectedClient(args.jwt)
 
 # Create a BinInfoProvider instance
 binPro = preprocessor_bosch.BinInfoProvider()
+if args.resume:
+    print("Resuming accumulated data....")
+    load_data(binPro)
 
 # buffer for mqtt messages in case of connection loss or timeout
 telemetry_queue = []
@@ -175,6 +226,10 @@ while True:
     
         # 7. MQTT: Send telemetry to the cloud. (in a JSON format)
         send_telemetry(args.host, args.port, comb, telemetry_queue)
+        
+        # 8. Saving telemetry dictionary
+        save_data(binPro)
+        
     # B. Do not sample data if the aftertreatment system is not ready
     else:
         print("\n# One or both NOx sensors is(are) not ready (default value: 3012.75).\n# No bin sampling.")
